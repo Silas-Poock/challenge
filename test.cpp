3ba50e40e76edb8366d2180e1bf79ef0e7afc8f1
Silas Poock <silas.poock@gmail.com>
2025-02-23 15:54:13 -0800

    playing around

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+
+using namespace std;
+
+int main() {
+    int bananas = 0;
+    int bites = 0;
+    
+
+}
