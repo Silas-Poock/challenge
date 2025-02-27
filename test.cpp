f96ba6fad7e13b84fe6af3d76e570455b3d0cae9
Silas Poock <silas.poock@gmail.com>
2025-02-27 15:35:19 -0800

    New Example

--- test.cpp
+++ test.cpp
@@ -9,10 +9,21 @@ using namespace std;
 
 int main() {
     int bananas = 0;
-    int bites = 0;
-<<<<<<< HEAD
+    int apples = 0;
+	int saleAmount = 0;
     
-    cin >> bananas >> bites;
-    cout << bananas << " " << bites << endl;
+    cin >> bananas >> apples;
+    cout << bananas << " " << apples << endl;
+
+    if (bananas > apples) {
+        saleAmount = 5;
+    }
+    else {
+		saleAmount = 3;
+    }
+
+    if (saleAmount > 4) {
+		cout << "sale was based on the number of bananas" << endl;
+    }
     return 0;
 }
